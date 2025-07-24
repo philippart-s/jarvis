fdf42396b89824c3199116f76e20c03983d1fbc3
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2025-07-24 15:33:43 +0200

    feat: ✨ Message for JUG Summer Camp

--- 33-jarvis-ai-run.sh
+++ 33-jarvis-ai-run.sh
@@ -8,4 +8,4 @@ bat -P -r 10:11 $(basename "$0")
 read -n 1 -p "Press any key to continue"
 
 # Run native executable with AI call 🚀
-jarvis-ai "Peux-tu me donner des informations sur la ville de Clermont-Ferrand?"
+jarvis-ai "Peux-tu me donner des informations sur la ville de La Rochelle ?"
