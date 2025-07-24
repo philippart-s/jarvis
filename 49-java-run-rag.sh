49e64eefe826cdf5fb6ef7c2e351fbf188017b66
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2025-07-24 17:19:17 +0200

    doc: 📝 Add explanation for RAG issue

--- 49-java-run-rag.sh
+++ 49-java-run-rag.sh
@@ -7,7 +7,6 @@ bat -P -r 10:11 $(basename "$0")
 
 read -n 1 -p "Press any key to continue"
 
-# Run the executable jar
-java -jar ./target/quarkus-app/quarkus-run.jar "Quel est le programme du Lava JUG le 22 mai ?"
+# Run the executable jar, why Java version? See: https://github.com/quarkiverse/quarkus-langchain4j/issues/1490
+java -jar ./target/quarkus-app/quarkus-run.jar "Quel est le programme du JUG Summer Camp le 5 septembre 2025 ?"
 echo ""
-
