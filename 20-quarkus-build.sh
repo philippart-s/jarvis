28da4480d0228bd93c4e337adbcee6f492111e5a
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2025-07-24 15:16:54 +0200

    fix: 🐛 Java 24 warnings

--- 20-quarkus-build.sh
+++ 20-quarkus-build.sh
@@ -1,9 +1,12 @@
 # 🛠️ Load environment variables 🛠️
 source .env
 
+# Deactivate warning du to Java 24
+export JDK_JAVA_OPTIONS='--add-opens java.base/java.lang=ALL-UNNAMED --enable-native-access=ALL-UNNAMED'
+
 clear
 
-bat -P -r 10:11 $(basename "$0")
+bat -P -r 13:14 $(basename "$0")
 
 read -n 1 -p "Press any key to continue"
 
