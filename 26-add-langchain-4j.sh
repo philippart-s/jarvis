5cacb12c8df9c8e57534e590aa72381287c223b8
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2025-12-17 17:43:27 +0100

    feat: ⬆️ Quarkus & extensions upgrade

--- 26-add-langchain-4j.sh
+++ 26-add-langchain-4j.sh
@@ -8,4 +8,4 @@ bat -P -r 10:11 $(basename "$0")
 read -n 1 -p "Press any key to continue"
 
 # Add LangChain4J extension
-quarkus ext add io.quarkiverse.langchain4j:quarkus-langchain4j-mistral-ai:1.1.0
+quarkus ext add io.quarkiverse.langchain4j:quarkus-langchain4j-mistral-ai:1.4.2
