8766f6dc0ff5fcacabbcc42199e0d1b9ef3ad670
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2025-12-19 15:20:23 +0100

    fix: 🐛 RAG with native mode

---
+++ src/main/java/fr/wilda/picocli/sdk/ai/InMemoryEmbeddingStoreProvider.java
@@ -0,0 +1,16 @@
+package fr.wilda.picocli.sdk.ai;
+
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+
+@ApplicationScoped
+public class InMemoryEmbeddingStoreProvider {
+  @Produces
+  @ApplicationScoped
+  public EmbeddingStore<TextSegment> embeddingStore() {
+    return new InMemoryEmbeddingStore<>();
+  }
+}
