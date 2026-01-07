f0bfb1180a567d8d572e44588638e039b8f2bba1
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2026-01-07 11:27:10 +0100

    clean: 🗑️ remove scripts

--- 40-emotion-prompt.sh
+++
@@ -1,9 +0,0 @@
-# 🛠️ Load environment variables 🛠️
-source .env
-
-clear
-
-bat -P -r 8: $(basename "$0")
-
-# Run native executable with AI call 🚀
-jarvis-ai-sentiment sentiment -s "I'm happy to be here"
