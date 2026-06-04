d7f2ab7fa0c4a6eb2daedf260240648a6f98b48d
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2026-06-04 11:18:06 +0200

    feat: 🗃️ new RAG use case (conference)

--- bin/67-run-agent-workflow.sh
+++ bin/67-run-agent-workflow.sh
@@ -21,9 +21,9 @@ bat -P -r 23:24 $(basename "$0")
 read -n 1 -p "Press any key to continue"
 
 # Run native executable with AI call 🚀
-jarvis-agents workflow "Avec les documents en ta possession, quelles sont les meilleures rillettes du monde?"
-#../target/jarvis-0.0.1-SNAPSHOT-runner workflow "Avec les documents en ta possession, quelles sont les meilleures rillettes du monde?"
-#java -jar ../target/quarkus-app/quarkus-run.jar workflow "Avec les documents en ta possession, quelles sont les meilleures rillettes du monde?"
+jarvis-agents workflow "Avec les documents en ta possession, Donne moi le programme du mardi 9 juin du Paris JUG"
+#../target/jarvis-0.0.1-SNAPSHOT-runner workflow "Avec les documents en ta possession, Donne moi le programme du mardi 9 juin du Paris JUG"
+#java -jar ../target/quarkus-app/quarkus-run.jar workflow "Avec les documents en ta possession, Donne moi le programme du mardi 9 juin du Paris JUG"
 
 read -n 1 -p "Press any key to continue"
 
