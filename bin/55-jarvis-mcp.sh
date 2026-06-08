f50126f5d1f02432791f0ac07d1b526c7dcfd423
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2026-06-08 10:59:55 +0200

    fix: 🐛 remove without mcp call

--- bin/55-jarvis-mcp.sh
+++ bin/55-jarvis-mcp.sh
@@ -3,19 +3,7 @@ source ../.env
 
 clear
 
-bat -P -r 10:11 $(basename "$0")
-
-read -n 1 -p "Press any key to continue"
-
-# Run native executable without MCP tools 🚀
-jarvis-mcp "Donne moi la liste de mes projet public cloud OVHCloud"
-#../target/jarvis-0.0.1-SNAPSHOT-runner "Donne moi la liste de mes projet public cloud OVHCloud"
-
-read -n 1 -p "Press any key to continue"
-
-clear
-
-bat -P -r 20:21 $(basename "$0")
+bat -P -r 8:9 $(basename "$0")
 
 # Run native executable with MCP tools 🚀
 jarvis-mcp mcp "Donne moi la liste de mes projet public cloud OVHCloud"
